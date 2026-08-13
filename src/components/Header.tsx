0f6dc2810d2be39bb1fd07c56f60e838c6640c5d
raaz4n <raazandoski@gmail.com>
2026-08-13 15:16:08 -0500

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,30 @@
-<>
-    <header className="header">
-        <div className="overlay"></div>
-    </header>
-    <div className="brand">
-        <div className="raz">
-            raaz4n
-        </div>
-    </div>
-    <div className="nav">
-        <div className="link">
-            ~/
-        </div>
-        <div className="link">
-            ~/blog
-        </div>
-        <div className="link">
-            ~/art
-        </div>
-        <div className="link">
-            ~/projects
-        </div>
-    </div>
-</>
+"use client"
+
+export default function Header() {
+    return (
+        <>
+            <header className="header">
+                <div className="overlay"></div>
+            </header>
+            <div className="brand">
+                <div className="raz">
+                    raaz4n
+                </div>
+            </div>
+            <div className="nav">
+                <div className="link">
+                    ~/
+                </div>
+                <div className="link">
+                    ~/blog
+                </div>
+                <div className="link">
+                    ~/art
+                </div>
+                <div className="link">
+                    ~/projects
+                </div>
+            </div>
+        </>
+    )
+}
